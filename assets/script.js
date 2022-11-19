8795279c34cd7fbbfcb0751fbef66d5aebe06eaa
Laidybird <predaveronica72@yahoo.com>
2022-11-19 12:09:04 +0000

    Configure js code

--- assets/script.js
+++ assets/script.js
@@ -7,23 +7,23 @@ let countryInp = document.getElementById("country-inp");
  * Creat the event listener
  */
 
-searchBtn.addEventListener("click", () => {
-    let countryName = countryInp.value;
-    let finalURL =`https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL);
-    fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies)[0]);
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(Object.values(data[0].languages).toString().split(",").join(","));
-        console.log(data[0].timezones[0])
-        results.innerHTML = `
+searchBtn.addEventListener("click", function () {
+        let countryName = countryInp.value;
+        let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+        console.log(finalURL);
+        fetch(finalURL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data[0]);
+                console.log(data[0].capital[0]);
+                console.log(data[0].flags.svg);
+                console.log(data[0].name.common);
+                console.log(data[0].continents[0]);
+                console.log(Object.keys(data[0].currencies)[0]);
+                console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+                console.log(Object.values(data[0].languages).toString().split(",").join(","));
+                console.log(data[0].timezones[0]);
+                results.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
         <h2>${data[0].name.common}</h2>
         <div class="wrapper">
@@ -54,9 +54,9 @@ searchBtn.addEventListener("click", () => {
            <div class="data-wrapper">
               <h4>Common Languages:</h4>
               <span>${Object.values(data[0].languages)
-                .toString()
-                .split(",")
-                .join(",")}</span>
+                        .toString()
+                        .split(",")
+                        .join(",")}</span>
             </div>
         </div>
         <div class="wrapper">
@@ -66,21 +66,21 @@ searchBtn.addEventListener("click", () => {
             </div>
         </div>
         `;
-        
-       
 
-        
-        
-    })
-/***
- * Creat a statement to specify what to do in case of an error
- */
-    .catch(() => {
-        if (countryName.length == 0){
-            results.innerHTML = `<h3>The input field cannot be empty</h3>`;
-        }
-        else {
-            results.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-        }
-    })
-});
+
+
+
+
+            })
+            /***
+             * Creat a statement to specify what to do in case of an error
+             */
+            .catch(() => {
+                if (countryName.length == 0) {
+                    results.innerHTML = `<h3>The input field cannot be empty</h3>`;
+                }
+                else {
+                    results.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+                }
+            });
+    });
